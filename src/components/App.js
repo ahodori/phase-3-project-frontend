15c8b83aa52c105739fddae9e182661b0c2758d9
Brody Losh <brodylosh@gmail.com>
2022-05-31 18:11:40 -0400

    Created Component Structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Container />
+    </div>
+  );
+}
+
+export default App;
